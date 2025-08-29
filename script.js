ba3543cb48a7387290375ee2dde33eb7c7c25f39
Dineshvidiyala <154747381+Dineshvidiyala@users.noreply.github.com>
2025-08-29 15:40:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:3000/api';
+const API_BASE_URL = 'https://obj-backend-1mo3.onrender.com/api';
 
 document.getElementById('excelFile').addEventListener('change', handleFileUpload);
 document.getElementById('generateButton').addEventListener('click', generateQuestionPaper);
